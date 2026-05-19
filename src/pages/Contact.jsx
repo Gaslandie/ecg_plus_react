7c8e365f33ba3e6ba9a078d01ab9a7cd5690b7b9
Mohamed Gassama <gassma803@gmail.com>
2026-05-19 19:21:42 +0000

    ...

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -287,7 +287,7 @@ const Contact = () => {
             <div className="map-responsive rounded shadow-lg">
               <iframe
                 src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15758.121696245131!2d-13.628965751911964!3d9.695383188582042!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xfa27e997a9f936f%3A0xc39115c2d3858055!2sCoyah%2C%20Guin%C3%A9e!5e0!3m2!1sfr!2sca!4v1701547746419!5m2!1sfr!2sca"
-                width="100%" height="450" style={{border:0}} allowFullScreen="" loading="lazy"
+                width="100%" height="225" style={{border:0}} allowFullScreen="" loading="lazy"
                 referrerPolicy="no-referrer-when-downgrade"></iframe>
             </div>
           </div>
